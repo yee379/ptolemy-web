0b71c6f1f6e6ca6c45ca7e65d6e2684861ab81ad
Yee-Ting Li <yee379@gmail.com>
2015-07-28 03:27:27 -0700

    added layout handler

--- app/controllers/d3s_controller.rb
+++ app/controllers/d3s_controller.rb
@@ -1,10 +1,18 @@
 class D3sController < ApplicationController
   before_action :set_params
   
+  def horizon
+    if params.has_key? :layout and params[:layout] == 'naked'
+      @layout = params[:layout]
+      render 'd3s/horizon', :layout => params[:layout]
+    end
+  end
+  
+  
   private
 
     def set_params
-      params.permit(:metric,:device,:physical_port,:source,:target)
+      params.permit(:metric,:device,:physical_port,:source,:target,:axis_on,:layout)
     end
 
 end
