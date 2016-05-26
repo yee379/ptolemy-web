9c299863f2a57f895e6307a41972aa2a7f04eb2c
Yee-Ting Li <yee379@gmail.com>
2016-05-26 13:03:14 -0700

    use grafana

--- app/controllers/grafanas_controller.rb
+++ app/controllers/grafanas_controller.rb
@@ -7,13 +7,21 @@ class GrafanasController < ApplicationController
   
   def port
     # add the neighbour port information if displaying ports
+    @speed = 1000
     if params.has_key? :metric and params[:metric] == 'port'
       ports = Port.where( :device => params[:device], :physical_port => stitch_port( params ) )
       @extra = pformat_neighbours( ports )
+      ports.each do |p|
+        # logger.warn "PORT %s" % (p.data['speed'])
+        @speed = p.data['speed']
+      end
     end
+    # get the port speed
     @fqdn = params[:device]
     @device = params[:device].split('.').reverse!.join('.')
     @port = stitch_port( params, delimiter='.' )
+    @speed = @speed.to_i * 1000000
+    @packets = @speed / 64
   end
   
   private
